3c4cf4ac1667b35ef83bf6af3dcd15fd6c4f398b
RX-kabir <156705525+RX-kabir@users.noreply.github.com>
2025-08-28 11:17:28 +0600

    Added Configuration file

---
+++ Backend/UniSpace/src/main/java/com/techManiacs/UniSpace/config/SpringSecurity.java
@@ -0,0 +1,52 @@
+package com.techManiacs.UniSpace.config;
+
+import com.techManiacs.UniSpace.filter.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+
+//    @Autowired
+//    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.cors().and()
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/public/**", "/api/classrooms", "/api/bookings/classSchedule/**").permitAll()
+                        .requestMatchers("/api/bookings/room/book").hasRole("CR")
+                        .requestMatchers("/student/my/bookings/**").hasRole("STUDENT")
+                        .requestMatchers("/api/bookings/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
+    }
+
+}
